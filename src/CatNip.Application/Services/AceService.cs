179a26be51191b2280eae8509a6166a1657859d3
Dimitrie Tataru <dimitriettr@gmail.com>
2026-07-20 15:22:49 +0300

    Integrate Import on Ace Service

--- src/CatNip.Application/Services/AceService.cs
+++ src/CatNip.Application/Services/AceService.cs
@@ -1,3 +1,5 @@
+using CatNip.Domain.ImportExport;
+using CatNip.Domain.ImportExport.Csv;
 using CatNip.Domain.Models.Interfaces;
 using CatNip.Domain.Query;
 using CatNip.Domain.Query.Filtering;
@@ -6,16 +8,20 @@ using CatNip.Domain.Services;
 
 namespace CatNip.Application.Services;
 
-public abstract class AceService<TRepository, TModel, TId, TFiltering>
-    : CrudService<TRepository, TModel, TId>, IAceService<TModel, TId, TFiltering>
-    where TRepository : IAceRepository<TModel, TId, TFiltering>
+public abstract class AceService<TRepository, TModel, TId, TFiltering, TExchange>
+    : CrudService<TRepository, TModel, TId>, IAceService<TModel, TId, TFiltering, TExchange>
+    where TRepository : IAceRepository<TModel, TId, TFiltering, TExchange>
     where TModel : IModel<TId>
     where TId : IEquatable<TId>
     where TFiltering : IFilteringRequest
+    where TExchange : ICsvMappable
 {
-    protected AceService(TRepository repository)
+    protected virtual ICsvConverter CsvConverter { get; init; }
+
+    protected AceService(TRepository repository, ICsvConverter csvConverter)
         : base(repository)
     {
+        CsvConverter = csvConverter;
     }
 
     public virtual async Task<QueryResponse<TModel>> GetAsync(
@@ -41,5 +47,24 @@ public abstract class AceService<TRepository, TModel, TId, TFiltering>
         int count = await Repository.CountAsync(filter, cancellation);
 
         return count;
+    }
+
+    public virtual async Task<ImportResponse> ImportAsync(ImportRequest request, CancellationToken cancellation = default)
+    {
+        ICollection<TExchange> importRecords;
+
+        try
+        {
+            importRecords = await CsvConverter.ReadAsync<TExchange>(request.Stream, cancellation);
+        }
+#pragma warning disable CA1031 // Do not catch general exception types
+        catch
+#pragma warning restore CA1031 // Do not catch general exception types
+        {
+            var failure = new ImportParseError($"Failed to read data from '{request.FileName}' file.");
+            return ImportResponse.Failure(failure);
+        }
+
+        return await Repository.ImportAsync(importRecords, cancellation);
     }
 }
